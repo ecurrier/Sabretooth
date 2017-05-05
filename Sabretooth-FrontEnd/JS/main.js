4bbaae7f1dfcb358571aa5fbcc3a7be81d749190
ecurrier <ecurri3@gmail.com>
2017-05-05 00:53:08 -0500

    Merged registration/login into one page

--- Sabretooth-FrontEnd/JS/main.js
+++ Sabretooth-FrontEnd/JS/main.js
@@ -1,12 +1,51 @@
+var currentForm;
+var loginTab;
+var registerTab;
+var loginForm;
+var registerForm;
+
 function loginUser() {
-    alert("Login User");
+    //alert("Login User");
+}
+
+function switchForm() {
+    if(currentForm === "login"){
+        registerForm.delay(150).fadeIn(150);
+ 		loginForm.fadeOut(150);
+
+        loginTab.removeClass("selectorActive");
+        registerTab.addClass("selectorActive");
+        currentForm = "register";
+    }
+    else {
+        loginForm.delay(150).fadeIn(150);
+ 		registerForm.fadeOut(150);
+
+        registerTab.removeClass("selectorActive");
+        loginTab.addClass("selectorActive");
+
+        currentForm = "login";
+    }
 }
 
 function attachHandlers() {
     $("#modalLoginButton").click(loginUser);
+    $("#selectorLogin").click(switchForm);
+    $("#selectorRegister").click(switchForm);
+}
+
+function initializeVariables() {
+    currentForm = "login";
+
+    loginTab = $("#selectorLogin");
+    loginForm = $("#modalLoginForm");
+
+    registerTab = $("#selectorRegister");
+    registerForm = $("#modalRegisterForm");
 }
 
 function onLoad() {
+    initializeVariables();
     attachHandlers();
 }
 
